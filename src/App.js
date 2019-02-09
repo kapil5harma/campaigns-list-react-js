289c4d29501d4ab575d193fc5dcdfe2ace89bdae
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-02-09 11:19:19 +0530

    - Basic component design - Dialog and Backdrop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
 
 class App extends Component {
+  state = {
+    listArr: [],
+    showDialog: false,
+    inputValue: null
+  };
+
+  showOrCloseDialog = () => {
+    console.log('this.state: ', this.state);
+    this.setState(currentState => {
+      return { showDialog: !currentState.showDialog };
+    });
+  };
+  createNewItem = () => {
+    console.log('Came here');
+    this.setState(
+      currentState => {
+        return { showDialog: !currentState.showDialog };
+      },
+      () => {
+        console.log('In callback');
+        console.log('this.state: ', this.state);
+
+        let newItem = {
+          campaignName: this.state.inputValue,
+          createdAt: new Date().toString().substr(16, 8)
+        };
+        let listArr = [...this.state.listArr, newItem];
+        console.log('listArr: ', listArr);
+        this.setState({ listArr });
+      }
+    );
+  };
+
+  handleInputChange = e => {
+    // console.log(e.target.value);
+    this.setState({ inputValue: e.target.value });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='title'>
+          <i className='far fa-envelope' />
+          <span className='on-icons-right'>All Campaigns</span>
+        </div>
+        <div className='head'>
+          <i className='fas fa-clipboard-list' />
+          <span className='on-icons-right'>All Campaigns</span>
+          <button onClick={() => this.showOrCloseDialog()} className='on-icons-right'>
+            + Create New
+          </button>
+        </div>
+        <div className='list-and-history'>
+          <div className='list'>
+            <List listArr={this.state.listArr} />
+          </div>
+          <div className='history'>
+            <div className='head'>
+              <i className='fas fa-history' />
+              <span className='on-icons-right'>History</span>
+            </div>
+          </div>
+        </div>
+        {this.state.showDialog && (
+          <div className='backdrop' onClick={() => this.showOrCloseDialog()}>
+            <div className='dialog' onClick={e => e.stopPropagation()}>
+              <label>Enter Campaign Name: </label>
+              <input type='text' onChange={e => this.handleInputChange(e)} />
+              <button onClick={() => this.createNewItem()}>Create</button>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
